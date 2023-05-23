cec9f93a9e23e22a48ac2fb421a9a09e5b60c1cd
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-05-23 12:17:40 +0200

    Add navbar for desktop version

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function () {
-  const navbar = document.querySelector('.header');
-  if (this.window.pageYOffset > 0) {
-    navbar.classList.add('scrolled');
-  } else {
-    navbar.classList.remove('scrolled');
-  }
-});
