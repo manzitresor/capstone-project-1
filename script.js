e2c619c1bf4f5acaf527f09700c38b28c6192a32
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-05-23 20:50:48 +0200

    Add artwork dynamically in javascript

--- script.js
+++ script.js
@@ -13,3 +13,44 @@ closeBtn.addEventListener('click', (event) => {
   menuList.style.display = 'none';
   menuButton.style.display = 'block';
 });
+
+const artwork = [
+  {
+    image: './images/pexels-steve-johnson-1266808.jpg',
+    name: 'Contemprary art',
+    descr: 'Spanning from 1970 to the present day.',
+  },
+  {
+    image: './images/abstract-city-art-drawing-qukwbza93wri3pnf.jpg',
+    name: 'Pop Art',
+    descr: 'Spanning from 1970 to the present day.',
+  },
+  {
+    image: './images/il_570xN.2967194173_n5h1.jpg',
+    name: 'Post-War',
+    descr: 'Spanning from 1970 to the present day.',
+  },
+  {
+    image: './images/desktop-wallpaper-mother-and-baby-painting-at-paintingvalley-mother-and-baby-painting.jpg',
+    name: 'Old Masters',
+    descr: 'Spanning from 1970 to the present day.',
+  },
+  {
+    image: './images/Screen-Shot-2019-12-20-at-16.08.24-1650x925.png',
+    name: 'Design',
+    descr: 'Spanning from 1970 to the present day.',
+  },
+];
+
+const cardContainer = document.querySelector('.card-container');
+
+artwork.forEach((project) => {
+  const artworkHTML = `
+    <div class="Contemporary">
+    <img src="${project.image}" alt="Contemporary-image" class="Contemporary-image">
+    <h2 class="contemporary-heading">${project.name}</h2>
+    <p class="contemporary-paragraph">${project.descr}</p>
+    </div>
+    `;
+  cardContainer.innerHTML += artworkHTML;
+});
